4cea259c0e0a2ff93a2d2d36c589f80a50aa4586
Rafgame38-prog <rafik.bouchenna@etu.univ-lyon1.fr>
2025-06-19 09:06:22 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,7 +16,7 @@ export async function verifyToken(req, res, next) {
 export function checkRole(role) {
   return async (req, res, next) => {
     if (!req.user || req.user.role !== role) {
-      return res.status(403).json({ message: "Accès refusé." });
+      return res.status(403).json({ message: "Accès refuser" });
     }
     next();
   };
